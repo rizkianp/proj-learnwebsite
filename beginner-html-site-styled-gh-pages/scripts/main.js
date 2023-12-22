23074b71b768fb2b5f5c35f4444612a9634768d9
rizand <rizkianpbiz@gmail.com>
2023-12-22 07:34:36 +0700

    experimenting with main.js

--- beginner-html-site-styled-gh-pages/scripts/main.js
+++ beginner-html-site-styled-gh-pages/scripts/main.js
@@ -1,2 +1,17 @@
 const myHeading = document.querySelector("h1");
 myHeading.textContent = "Hello World!";
+
+const myImage = document.querySelector("img");
+
+myImage.onclick = () => {
+    const mySrc = myImage.getAttribute("src");
+    if (mySrc === "images/firefox-icon.png") {
+        let changesize = document.getElementById("homes");
+        changesize.height = 256;
+        changesize.width = 256;
+        myImage.setAttribute("src", "images/firefox2.jpg");
+    }
+        else {
+            myImage.setAttribute("src", "images/firefox-icon.png" );
+        }
+    };
